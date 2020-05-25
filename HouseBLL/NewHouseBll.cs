8a7315899042065a700a3280100187b417ee9ec3
1720934884@qq.com <1720934884@qq.com>
2020-05-25 14:51:17 +0800

    新房源

---
+++ HouseBLL/NewHouseBll.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HouseDAL;
+using HouseModel;
+namespace HouseBLL
+{
+   public class NewHouseBll
+    {
+        NewHouseDal dal = new NewHouseDal();
+        //添加新房源
+        public int AddHouse(HouseCollectModel house)
+        {
+            return dal.AddHouse(house);
+        }
+    }
+}
---
+++ HouseDAL/NewHouseDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HouseModel;
+namespace HouseDAL
+{
+      public  class NewHouseDal
+    {
+        //添加新房源
+        public int AddHouse(HouseCollectModel house)
+        {
+            string sql = $"insert into HouseCollect values({house.HouseType},'{house.HouseTenement}',{house.HouseSum},'{house.HouseSite}',{house.HousePrice},{house.HouseNumber},'{house.HouseName}',{house.HouseModel},'{house.HouseIntro}','{house.HouseInfo}','{house.HouseImage}',{house.HouseGradation},{house.HouseArea}) ";
+            return DapperHelper<HouseCollectModel>.Execute(sql, null);
+        }
+    }
+}
